c9db3e91ee01806be692f15a7ecbed830f9a74ff
= <thao.ngo@student.passerellesnumeriques.org>
2019-05-08 11:15:12 +0700

    [db thao]

--- app/Category.php
+++ app/Category.php
@@ -6,9 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-	
     protected $table ='categories';
-    protected $filable=['id','name'];
+    protected $filable=['name'];
     public $timestamps=true;
     //Du dung 2 truong created and updated 
     public function Products(){
